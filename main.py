70e3f0d11aa69cddeef98565d965ec216961b0cd
Jim8822 <georgestone379@gmail.com>
2026-06-08 05:48:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🏓 Table Tennis Over Alert Bot is running!")
+    await update.message.reply_text("🏓 Bot is running!")
 
-app= Application.builder().token(os.getenv("BOT_TOKEN")).build()
+app = Application.builder().token(os.environ["BOT_TOKEN"]).build()
 
 app.add_handler(CommandHandler("start", start))
 
